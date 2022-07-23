0604dd0981af0d1ef7a62d0e35c808e203eec81c
Anggara Setiawan <aanggaadc@gmail.com>
2022-07-24 02:38:00 +0700

    done create header and footer

---
+++ components/Layout/Footer.jsx
@@ -0,0 +1,67 @@
+import Image from 'next/image'
+import styles from './Footer.module.css'
+import Logo from '../../public/Logo.svg'
+import Icon from '../../public/Icon.svg'
+import Heart from '../../public/Heart.svg'
+
+export default function Footer() {
+    return (
+        <footer id={styles.footer}>
+            <div className={styles.top}>
+                <div className={styles.left}>
+                    <Image src={Logo} alt="Logo" />
+                    <h3>Build a modern and creative website with crealand</h3>
+                    <div className={styles.icon}>
+                        <Image src={Icon} alt="Icon" />
+                        <Image src={Icon} alt="Icon" />
+                        <Image src={Icon} alt="Icon" />
+                        <Image src={Icon} alt="Icon" />
+                    </div>
+                </div>
+                <div className={styles.right}>
+                    <div className={styles.product}>
+                        <h4>Product</h4>
+                        <ul>
+                            <li>Landingpage</li>
+                            <li>Features</li>
+                            <li>Documentation</li>
+                            <li>Referral Program</li>
+                            <li>Pricing</li>
+                        </ul>
+                    </div>
+                    <div className={styles.service}>
+                        <h4>Services</h4>
+                        <ul>
+                            <li>Documentation</li>
+                            <li>Design</li>
+                            <li>Themes</li>
+                            <li>Illustration</li>
+                            <li>Ui Kit</li>
+                        </ul>
+                    </div>
+                    <div className={styles.company}>
+                        <h4>Company</h4>
+                        <ul>
+                            <li>About</li>
+                            <li>Terms</li>
+                            <li>Privacy Policy</li>
+                            <li>Carrers</li>
+                        </ul>
+                    </div>
+                    <div className={styles.more}>
+                        <h4>More</h4>
+                        <ul>
+                            <li>Documentation</li>
+                            <li>License</li>
+                            <li>Changelog</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.bottom}>
+                <Image src={Heart} alt="heart" />
+                <p>Copyright © 2019. Crafted with love.</p>
+            </div>
+        </footer>
+    )
+}
---
+++ components/Layout/Header.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import Logo from '../../public/Logo.svg'
+import Image from 'next/image'
+import styles from './Header.module.css'
+import { BsList } from "react-icons/bs";
+
+export default function Header() {
+    const [mobileNav, setMobileNav] = useState(false)
+
+    return (
+        <header id={styles.header}>
+            <Image src={Logo} alt="Logo" />
+            <ul className={mobileNav ? `${styles.left.expanded}` : `${styles.left}`}>
+                <li><h4>See Open Jobs</h4> </li>
+                <li><button>Contact us</button></li>
+            </ul>
+            <div className={styles.menu_icon}>
+                <BsList color="#3A4C66" size={40} onClick={() => {
+                    setMobileNav(!mobileNav)
+                }} />
+            </div>
+        </header >
+    )
+}
